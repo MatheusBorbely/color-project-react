43a3b799bb4f821ec06430e513f951ed2ee8f5f5
MatheusBorbely <matheus.silvaborbely@gmail.com>
2022-10-25 18:18:44 -0300

    Adicionando Select componente dentro do NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,12 @@ import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./NavBar.css";
 
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
 class NavBar extends Component{
     constructor(props){
         super(props);
@@ -32,7 +38,13 @@ class NavBar extends Component{
                         </div>
                     </div>
                     <div className="changeColor-container">
+                        <Select>
 
+                            <MenuItem value="hex">HEX - #FFFFF</MenuItem>
+                            <MenuItem value="rgb">RGB- rgb(255,255,255)</MenuItem>
+                            <MenuItem value="rgba">RGBA- rgba(255,255,255, 1.0)</MenuItem>
+
+                        </Select>
                     </div>
                 </div>   
             </header>
